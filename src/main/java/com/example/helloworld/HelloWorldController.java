050bf038c3470932e63684f7ac2cfa9ac07ad646
Alok Jena <alokjen@hotmail.com>
2022-05-05 17:18:15 -0700

    first commit

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.example.helloworld;
 
+import com.example.model.Greeting;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 public class HelloWorldController {
 
+    private final AtomicInteger atom = new AtomicInteger();
+    public static final String format = "Hello %s";
+
     @GetMapping("/greeting")
-    public String hello(@RequestParam(value="name", defaultValue="world") String name) {
-        return "hello "+name;
+    public Greeting hello(@RequestParam(value="name", defaultValue="world") String name) {
+
+        return new Greeting(atom.incrementAndGet(), String.format(format, name));
     }
 }
